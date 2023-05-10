d5a548f383a2724e380a09824fbddb01c32ada1c
rudqls007 <rudqls007@gmail.com>
2023-05-10 20:28:54 +0900

    simple MVC

---
+++ src/main/java/hello/basics/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package hello.basics.repository;
+
+import hello.basics.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+
+    Member save(Member member);
+    Optional<Member> findByid(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
