04ac313a9df82d7c20392df19549645bfe1250d7
rudqls007 <rudqls007@gmail.com>
2023-05-16 19:41:24 +0900

    스프링 JdbcTemplate 순수 Jdbc와 동일한 환경설정을 하면 된다. 스프링 JdbcTemplate과 MyBatis 같은 라이브러리는 JDBC API에서 본 반복 코드를 대부분 제거해준다. 하지만 SQL은 직접 작성해야 한다.

--- src/main/java/hello/basics/service/SpringConfig.java
+++ src/main/java/hello/basics/service/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.basics.service;
 
 import hello.basics.repository.JdbcMemberRepository;
+import hello.basics.repository.JdbctemplateMemberRepository;
 import hello.basics.repository.MemberRepository;
 import hello.basics.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 
         //return new MemoryMemberRepository();
-        return  new JdbcMemberRepository(dataSource);
+        //return  new JdbcMemberRepository(dataSource);
+        return  new JdbctemplateMemberRepository(dataSource);
     }
 }
